f22b71a7e3e0938f800f13fd28aa47d9c82466bd
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-07 10:50:40 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Edmond Podlegaev on 07.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
