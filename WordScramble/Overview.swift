f727f934705bfeca2fd3dc15c6153e17c6f4f778
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-07 10:52:07 +0300

    Add Overview

---
+++ WordScramble/Overview.swift
@@ -0,0 +1,20 @@
+//
+//  Overview.swift
+//  WordScramble
+//
+//  Created by Edmond Podlegaev on 07.04.2023.
+//
+
+import SwiftUI
+
+struct Overview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Overview_Previews: PreviewProvider {
+    static var previews: some View {
+        Overview()
+    }
+}
