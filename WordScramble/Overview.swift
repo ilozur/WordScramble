d5e61078aff81a4a460b1646adc3393527544e9f
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-07 11:37:45 +0300

    Working with strings

--- WordScramble/Overview.swift
+++ WordScramble/Overview.swift
@@ -49,6 +49,32 @@ struct Overview: View {
         }
     }
     
+    func newTest() {
+        var input = "a b c"
+        var letters = input.components(separatedBy: " ")
+        
+        input = """
+                a
+                b
+                c
+                """
+        
+        letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    func oldTest() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+    }
 }
 
 struct Overview_Previews: PreviewProvider {
